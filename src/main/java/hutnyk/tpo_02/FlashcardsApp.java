128c3c816cad1b9075c4398ab61e34c342f2717e
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2024-11-14 21:53:44 +0100

    modified main method

--- src/main/java/hutnyk/tpo_02/FlashcardsApp.java
+++ src/main/java/hutnyk/tpo_02/FlashcardsApp.java
@@ -1,30 +1,19 @@
 package hutnyk.tpo_02;
 
-import hutnyk.tpo_02.Model.IEntry;
-import hutnyk.tpo_02.Service.FileService;
+import hutnyk.tpo_02.Controller.FlashcardsController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 @SpringBootApplication
 public class FlashcardsApp {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(FlashcardsApp.class, args);
+        FlashcardsController controller = context.getBean(FlashcardsController.class);
+        controller.startApplication();
 
 
-//        List<IEntry> data = new ArrayList<>();
-//        FileService fileService = new FileService();
-//
-//        data = fileService.readFile("src/main/resources/dictionary.txt");
-//        for(IEntry entry : data){
-//            System.out.println(entry);
-//        }
 
 
     }
