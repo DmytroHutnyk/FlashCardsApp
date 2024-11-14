f10169a7460129bc3ed0cc923e272cf5e0ada5a2
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2024-11-14 16:23:16 +0100

    implemented basic logic of Repository class

---
+++ src/main/java/hutnyk/tpo_02/Repository/EntryRepository.java
@@ -0,0 +1,29 @@
+package hutnyk.tpo_02.Repository;
+
+import hutnyk.tpo_02.Model.IEntry;
+import hutnyk.tpo_02.Service.IReadWriteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class EntryRepository {
+    private List<IEntry> entries;
+
+    @Value("${hutnyk.tpo_02.filename}")
+    private String filename;
+
+    @Autowired
+    public EntryRepository(IReadWriteService service){
+        entries = service.readFile(filename);
+    }
+
+
+    public List<IEntry> getEntries() {
+        return entries;
+    }
+
+
+}
